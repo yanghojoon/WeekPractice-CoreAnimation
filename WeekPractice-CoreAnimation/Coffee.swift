812ac2f463c9f26f507d403ceec6b0b20136aac5
yanghojoon <yanghojoon8487@gmail.com>
2022-02-26 20:38:02 +0900

    feat: 빨대를 그리는 메서드 추가

--- WeekPractice-CoreAnimation/Coffee.swift
+++ WeekPractice-CoreAnimation/Coffee.swift
@@ -39,18 +39,23 @@ class Coffee: UIView {
     func drawStraw() {
         let path = UIBezierPath()
         path.move(to: CGPoint(x: frame.width * 0.95, y: frame.height * 0.1))
-        path.addLine(to: CGPoint(x: frame.width * 0.8, y: frame.height * 0.2))
-        path.addLine(to: CGPoint(x: frame.width * 0.27, y: frame.height * 0.7))
+        path.addLine(to: CGPoint(x: frame.width * 0.75, y: frame.height * 0.2))
+        path.addLine(to: CGPoint(x: frame.width * 0.3, y: frame.height * 0.7))
         
         let layer = CAShapeLayer()
         layer.frame = bounds
         layer.path = path.cgPath
         layer.strokeColor = UIColor.black.cgColor
+        layer.fillColor = UIColor.clear.cgColor
+        layer.lineWidth = 15
+        
+        self.layer.addSublayer(layer)
     }
     
     override func layoutMarginsDidChange() { // bounds가 변하는 것을 감지. Rect가 갱신될 때마다 호출.
         self.backgroundColor = .white
         drawCup()
+        drawStraw()
     }
 }
 
