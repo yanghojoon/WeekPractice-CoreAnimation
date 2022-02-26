a0a2cc9a316b2e9e797ceb1acbf5c1ebe695e4bd
yanghojoon <yanghojoon8487@gmail.com>
2022-02-26 22:23:26 +0900

    feat: 컵 안에 있는 커피 구현
    - 기존 컵의 path대로 꽉 찬 커피를 구현
    - 사각형의 layer를 설정하여 coffeeLayer의 mask로 설정

--- WeekPractice-CoreAnimation/Coffee.swift
+++ WeekPractice-CoreAnimation/Coffee.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 
-class Coffee: UIView {
-    func drawCup() {
+final class Coffee: UIView {
+    private let cupLayer = CAShapeLayer()
+    private let coffeeLayer = CAShapeLayer()
+    private let leftCoffeeLayer = CAShapeLayer()
+    
+    private func makeCoffeeCupPath() -> UIBezierPath {
         let path = UIBezierPath()
         path.move(to: CGPoint(x: frame.width * 0.15, y: frame.height * 0.2))
         path.addLine(to: CGPoint(
@@ -26,17 +30,23 @@ class Coffee: UIView {
             y: frame.height * 0.2)
         )
         
-        let layer = CAShapeLayer()
-        layer.frame = bounds
-        layer.path = path.cgPath
-        layer.strokeColor = UIColor.systemGray.cgColor
-        layer.fillColor = UIColor.clear.cgColor
-    
-        layer.lineWidth = 15
-        self.layer.addSublayer(layer)
+        return path
     }
     
-    func drawStraw() {
+    private func drawCup() {
+        let path = makeCoffeeCupPath()
+        
+        cupLayer.frame = bounds
+        cupLayer.path = path.cgPath
+        cupLayer.strokeColor = UIColor.systemGray.cgColor
+        cupLayer.fillColor = UIColor.clear.cgColor
+        cupLayer.lineWidth = 15
+        cupLayer.masksToBounds = false
+        
+        layer.addSublayer(cupLayer)
+    }
+    
+    private func drawStraw() {
         let path = UIBezierPath()
         path.move(to: CGPoint(x: frame.width * 0.95, y: frame.height * 0.1))
         path.addLine(to: CGPoint(x: frame.width * 0.75, y: frame.height * 0.2))
@@ -48,15 +58,40 @@ class Coffee: UIView {
         layer.strokeColor = UIColor.black.cgColor
         layer.fillColor = UIColor.clear.cgColor
         layer.lineWidth = 15
+        layer.lineCap = .round
         
         self.layer.addSublayer(layer)
     }
     
+    private func drawCoffee() {
+        let path = makeCoffeeCupPath()
+
+        coffeeLayer.frame = bounds
+        coffeeLayer.path = path.cgPath
+        coffeeLayer.fillColor = UIColor.brown.cgColor
+        coffeeLayer.lineWidth = 15
+        coffeeLayer.strokeColor = UIColor.clear.cgColor
+        coffeeLayer.mask = leftCoffeeLayer
+        
+        layer.addSublayer(coffeeLayer)
+    }
+    
+    private func drawLeftCoffee() {
+        let maskingRectangle = CGRect(x: .zero, y: frame.height * 0.3, width: frame.width, height: frame.height)
+        
+        leftCoffeeLayer.frame = bounds
+        leftCoffeeLayer.path = UIBezierPath(rect: maskingRectangle).cgPath
+    }
+    
     override func layoutMarginsDidChange() { // bounds가 변하는 것을 감지. Rect가 갱신될 때마다 호출.
-        self.backgroundColor = .white
-        drawCup()
+        backgroundColor = .systemBackground
+        
         drawStraw()
+        drawLeftCoffee()
+        drawCoffee()
+        drawCup()
     }
 }
 
 // shift + cmd + a -> 다크모드
+// mask로 보이고 싶은 부분을 정의한다.
